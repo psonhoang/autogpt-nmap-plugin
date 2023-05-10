3a7012c237f01feda69d8e72603f436087a2d843
Hoang S Pham <pshoang102@gmail.com>
2023-05-10 17:03:26 -0500

    remove abstract decorators

--- src/auto-gpt-nmap-plugin/__init__.py
+++ src/auto-gpt-nmap-plugin/__init__.py
@@ -23,7 +23,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
         self._version = "0.1.0"
         self._description = "A simple plugin wrapped around python-nmap to scan ports"
 
-    @abc.abstractmethod
     def can_handle_on_response(self) -> bool:
         """This method is called to check that the plugin can
         handle the on_response method.
@@ -32,12 +31,10 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
             bool: True if the plugin can handle the on_response method."""
         return False
 
-    @abc.abstractmethod
     def on_response(self, response: str, *args, **kwargs) -> str:
         """This method is called when a response is received from the model."""
         pass
 
-    @abc.abstractmethod
     def can_handle_post_prompt(self) -> bool:
         """This method is called to check that the plugin can
         handle the post_prompt method.
@@ -46,7 +43,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
             bool: True if the plugin can handle the post_prompt method."""
         return False
 
-    @abc.abstractmethod
     def post_prompt(self, prompt: PromptGenerator) -> PromptGenerator:
         """This method is called just after the generate_prompt is called,
             but actually before the prompt is generated.
@@ -71,7 +67,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
 
         return prompt
 
-    @abc.abstractmethod
     def can_handle_on_planning(self) -> bool:
         """This method is called to check that the plugin can
         handle the on_planning method.
@@ -80,7 +75,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
             bool: True if the plugin can handle the on_planning method."""
         return False
 
-    @abc.abstractmethod
     def on_planning(
         self, prompt: PromptGenerator, messages: List[Message]
     ) -> Optional[str]:
@@ -92,7 +86,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_post_planning(self) -> bool:
         """This method is called to check that the plugin can
         handle the post_planning method.
@@ -101,7 +94,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
             bool: True if the plugin can handle the post_planning method."""
         return False
 
-    @abc.abstractmethod
     def post_planning(self, response: str) -> str:
         """This method is called after the planning chat completion is done.
 
@@ -113,7 +105,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_pre_instruction(self) -> bool:
         """This method is called to check that the plugin can
         handle the pre_instruction method.
@@ -122,7 +113,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
             bool: True if the plugin can handle the pre_instruction method."""
         return False
 
-    @abc.abstractmethod
     def pre_instruction(self, messages: List[Message]) -> List[Message]:
         """This method is called before the instruction chat is done.
 
@@ -134,7 +124,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_on_instruction(self) -> bool:
         """This method is called to check that the plugin can
         handle the on_instruction method.
@@ -143,7 +132,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
             bool: True if the plugin can handle the on_instruction method."""
         return False
 
-    @abc.abstractmethod
     def on_instruction(self, messages: List[Message]) -> Optional[str]:
         """This method is called when the instruction chat is done.
 
@@ -155,7 +143,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_post_instruction(self) -> bool:
         """This method is called to check that the plugin can
         handle the post_instruction method.
@@ -164,7 +151,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
             bool: True if the plugin can handle the post_instruction method."""
         return False
 
-    @abc.abstractmethod
     def post_instruction(self, response: str) -> str:
         """This method is called after the instruction chat is done.
 
@@ -176,7 +162,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_pre_command(self) -> bool:
         """This method is called to check that the plugin can
         handle the pre_command method.
@@ -185,7 +170,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
             bool: True if the plugin can handle the pre_command method."""
         return False
 
-    @abc.abstractmethod
     def pre_command(
         self, command_name: str, arguments: Dict[str, Any]
     ) -> Tuple[str, Dict[str, Any]]:
@@ -200,7 +184,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_post_command(self) -> bool:
         """This method is called to check that the plugin can
         handle the post_command method.
@@ -209,7 +192,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
             bool: True if the plugin can handle the post_command method."""
         return False
 
-    @abc.abstractmethod
     def post_command(self, command_name: str, response: str) -> str:
         """This method is called after the command is executed.
 
@@ -222,7 +204,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_chat_completion(
         self, messages: Dict[Any, Any], model: str, temperature: float, max_tokens: int
     ) -> bool:
@@ -239,7 +220,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
               bool: True if the plugin can handle the chat_completion method."""
         return False
 
-    @abc.abstractmethod
     def handle_chat_completion(
         self, messages: List[Message], model: str, temperature: float, max_tokens: int
     ) -> str:
@@ -256,7 +236,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_text_embedding(
         self, text: str
     ) -> bool:
@@ -268,7 +247,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
               bool: True if the plugin can handle the text_embedding method."""
         return False
 
-    @abc.abstractmethod
     def handle_text_embedding(
         self, text: str
     ) -> list:
@@ -280,7 +258,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_user_input(self, user_input: str) -> bool:
         """This method is called to check that the plugin can
         handle the user_input method.
@@ -292,7 +269,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
             bool: True if the plugin can handle the user_input method."""
         return False
 
-    @abc.abstractmethod
     def user_input(self, user_input: str) -> str:
         """This method is called to request user input to the user.
 
@@ -305,7 +281,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
 
         pass
 
-    @abc.abstractmethod
     def can_handle_report(self) -> bool:
         """This method is called to check that the plugin can
         handle the report method.
@@ -314,7 +289,6 @@ class AutoGPTNmapPlugin(AutoGPTPluginTemplate):
             bool: True if the plugin can handle the report method."""
         return False
 
-    @abc.abstractmethod
     def report(self, message: str) -> None:
         """This method is called to report a message to the user.
 
